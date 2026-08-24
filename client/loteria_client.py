f3cb8b43c0bd77992969b9d6a2c9c874079ed75d
Vinicius <viniciusdemoraeschaves@gmail.com>
2026-08-24 00:25:33 -0300

    feat: first commit

---
+++ client/loteria_client.py
@@ -0,0 +1,18 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 5000
+
+#função referente a thread 1 que vai ler do teclado e enviar para o servidor
+def send_data(sock):
+   
+#referente a Thread 2 que vai ler e imprimir na tela
+def receive_data(sock):
+  
+
+def main():
+
+  
+if __name__ == "__main__":
+    main()
