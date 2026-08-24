99305c7d3631de2962bc0f29d3b1cbc885429872
felipe Cantarin <felpsmageguglio@gmail.com>
2026-08-24 20:00:17 -0300

    feat: implement base loteria client and send data

--- client/loteria_client.py
+++ client/loteria_client.py
@@ -4,15 +4,33 @@ import threading
 HOST = '127.0.0.1'
 PORT = 5000
 
-#função referente a thread 1 que vai ler do teclado e enviar para o servidor
+# função referente a thread 1 que vai ler do teclado e enviar para o servidor
 def send_data(sock):
-   
-#referente a Thread 2 que vai ler e imprimir na tela
-def receive_data(sock):
-  
+    while True:
+        try:
+            message = input("Digite sua mensagem: ")
+        except EOFError:
+            print("EOFError: Encerrando envio de dados.")
+            break
+        try:
+            sock.sendall((message + '\n').encode())
+            print(f"[ENVIADO] {message}")
+        except OSError as e:
+            print(f"Erro ao enviar dados: {e}")
+            break
 
 def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
 
-  
+    print("[CONFIGURACAO] :inicio <N>, :fim <N>, :qtd <N>")
+    print("[APOSTAR] números separados por espaço.\n")
+
+    tread1 = threading.Thread(target=send_data, args=(sock,), daemon=True)
+    tread1.start()
+    tread1.join()
+
+    sock.close()
+
 if __name__ == "__main__":
     main()
